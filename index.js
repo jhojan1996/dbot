3b99d4319bf6ec781e62b95c82a3ace0480f643e
jhojandev <jhojan@consultoriagp.co>
2017-07-10 16:09:30 -0500

    Flow dialogs

---
+++ index.js
@@ -0,0 +1,75 @@
+var builder = require('botbuilder');
+var restify = require('restify');
+
+
+//configura el servidor tipo restify
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
+});
+
+// Crea el nuevo bot y configura la escucha de mensajes
+// Las variables de entorno ".env" se cargan desde azure
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID, 
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+server.post('/api/messages', connector.listen());
+
+var DialogLabels = {
+    Rut: 'Crear rut',
+    Citas: 'Agendar una cita',
+    Login: 'Ingresar al sistema',
+    Ayuda: 'Solicitar ayuda'
+};
+
+//Este dialogo es para intent por defecto
+var bot = new builder.UniversalBot(connector, [
+    function (session) {
+        // prompt for search option
+        builder.Prompts.choice(
+            session,
+            'Hola! Soy DiBot. dime, ¿Qué deseas hacer?',
+            [DialogLabels.Ayuda, DialogLabels.Rut, DialogLabels.Login, DialogLabels.Citas],
+            {
+                maxRetries: 3,
+                retryPrompt: 'Por favor escoge o escribe una de las opciones'
+            });
+    },
+    function (session, result) {
+        if (!result.response) {
+            // exhausted attemps and no selection, start over
+            session.send('Lo siento! No pude atender tu solicitud. Pero no te preocupes, puede escribirlo de nuevo!');
+            return session.endDialog();
+        }
+
+        // on error, start over
+        session.on('error', function (err) {
+            session.send('Error interno, si persiste envie un e-mail a xxx@xxx.com con el siguiente mensaje: %s', err.message);
+            session.endDialog();
+        });
+
+        // continue on proper dialog
+        var selection = result.response.entity;
+        switch (selection) {
+            case DialogLabels.Ayuda:
+                return session.beginDialog('ayuda');
+            case DialogLabels.Rut:
+                return session.beginDialog('CrearRut');
+            case DialogLabels.Login
+            	return session.beginDialog('login');
+            case DialogLabels.Citas
+            	return session.beginDialog('citas');
+        }
+    }
+]);
+
+// Aquí instanciamos a LUIS
+var recognizer = new builder.LuisRecognizer(process.env.LUIS_MODEL_URL);
+bot.recognizer(recognizer);
+
+bot.dialog('Ayuda', require('./actions/ayuda'));
+bot.dialog('CrearRut', require('./actions/crearRut'));
+bot.dialog('GestionarRut', require('./actions/gestionarRut'));
+bot.dialog('Login', require('./actions/Login'));
+bot.dialog('CrearCita', require('.actions/crearCita'));
