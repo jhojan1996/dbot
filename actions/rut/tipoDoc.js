4ca193130fe965a19ef59db4ea82de3c4b9df4c5
Innovati1\Jhojan Garcia <jhojan@consultoriagp.co>
2017-07-11 14:55:47 -0500

    Crear rut dialog flow

--- actions/rut/tipoDoc.js
+++ actions/rut/tipoDoc.js
@@ -2,8 +2,9 @@ var builder = require('botbuilder');
 
 module.exports = [
 	function (session) {
+		var options = session.localizer.gettext(session.preferredLocale(), "es");
 		console.log("Ingresar tipo de documento ---- crear rut");
-        builder.Prompts.text(session, "Por favor dime tu tipo de documento, por ejemplo: Cédula");
+        builder.Prompts.text(session, "Por favor dime tu tipo de documento, por ejemplo: Cédula",options);
     },
     function (session, results) {
     	console.log("Tipo doc ----> ",results);
