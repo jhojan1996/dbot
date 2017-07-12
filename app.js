5496d85ca965834d27fb6b79914d21471a60e3dc
Innovati1\Jhojan Garcia <jhojan@consultoriagp.co>
2017-07-12 14:02:01 -0500

    Cancel actions

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var restify = require('restify');
 
 // Setup Restify Server
 var server = restify.createServer();
+server.use(restify.queryParser());
 server.listen(process.env.port || process.env.PORT || 3978, function () {
     console.log('%s listening to %s', server.name, server.url);
 });
