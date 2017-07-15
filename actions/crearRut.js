77350fcb7aff306912c5aa6aebf0d0c99fbfe547
jhojandev <jhojan@consultoriagp.co>
2017-07-15 18:10:22 -0500

    transaccion crearRut

--- actions/crearRut.js
+++ actions/crearRut.js
@@ -144,7 +144,7 @@ module.exports = [
 					});
 				}
 
-				var log = res.insertId;
+				var log = result.insertId;
 
 				connection.query('INSERT INTO detalle_usuario (id,id_usuario, tipo_documento, documento, fecha_exp, pais_exp, dpto_exp, mpio_exp, pais_ubi, dpto_ubi, mpio_ubi) VALUES (?,?,?,?,?,?,?,?,?,?,?)', ['',log, session.dialogData.tipoDocumento, session.dialogData.numeroDocumento, session.dialogData.fechaExpe, session.dialogData.paisExpe, session.dialogData.dptoExpe, session.dialogData.mpioExpe, session.dialogData.paisUbi, session.dialogData.dptoUbi, session.dialogData.mpioUbi], function(err, result) {
 					if (err) { 
