cdef49ba6d843d5a3d40cb75ba4be9b60a145e9c
Innovati1\Jhojan Garcia <jhojan@consultoriagp.co>
2017-07-31 16:30:34 -0500

    notificaciones

--- actions/notificar.js
+++ actions/notificar.js
@@ -1,8 +1,6 @@
 var builder = require('botbuilder');
 var schedule = require('node-schedule');
 var mysql = require('mysql');
-var not = false;
-var cuatrimestral;
 
 var connection = mysql.createConnection({
     host     : 'us-cdbr-azure-southcentral-f.cloudapp.net',
@@ -59,84 +57,60 @@ module.exports = [
 	function(session, result){
 		session.dialogData.horaNoti = result.response;
 		console.log("DATOS DE LA NOTIFICACION-------------->",session.dialogData);
-	
+
 		programarNoti(session);
+
 		session.endDialog("Su suscripción al servicio de notificaciones fue realizada correctamente.");
 	}
 ];
 
 function programarNoti(session){
-	var currentDate = new Date();
-	var nMonth = currentDate.getMonth();
-	var consulta;
-	var idUsuario = session.userData.idUsuario;
-
-	connection.beginTransaction(function(err) {
-		if (err) {
-			console.log("ERROR 1-------->",err); 
-			throw err; 
-		}
-		
-		connection.query('SELECT documento FROM detalle_usuario WHERE id_usuario = ?', idUsuario, function(err, result) {
-			console.log("ERROR: ----------------> "+err+" ||| RESULT ------------>:"+result);
-			if (err) { 
-				console.log("ERROR 2:------------>",err);
-				throw err;
-			}
-			if(result.length > 0){
-	            console.log("RESULT DOCUMENTO----------->", result);
-	            documento = result[0].documento;
-	            var lastChar = documento.substr(documento.length-1);
-	            var cuatrimestre;
-	            if(nMonth >= 0 && nMonth <= 3){
-					cuatrimestre = "1";
-				}else if(nMonth > 3 && nMonth <= 7){
-					cuatrimestre = "2";
-				}else{
-					cuatrimestre = "3";
-				}
-				connection.query("SELECT cuatrimestre"+cuatrimestre+" FROM declaracion WHERE ult_digito = ?",lastChar, function(err, result, fields) {
-	            	console.log("ERROR SEGUNDO QUERY---->",err);
-	        		if (err) throw err;
-	            	not = true;
-	            	 if(cuatrimestre === "1"){
-			        	cuatrimestre = result[0].cuatrimestre1;
-			        }else if(cuatrimestre === "2"){
-			        	cuatrimestre = result[0].cuatrimestre2;
-			        }else{
-			        	cuatrimestre = result[0].cuatrimestre3;
-			        }
-	            });
-			}
-			connection.end();
-		});
-	});
-
-	if (not){
-		console.log("termino programar noti");
-		noti();
-	}
-}
-function noti(){
-	var currentDate = new Date();
-	var nMonth = currentDate.getMonth();
-	var cuatrimestre;
-	if(nMonth >= 0 && nMonth <= 3){
-		cuatrimestre = "1";
-	}else if(nMonth > 3 && nMonth <= 7){
-		cuatrimestre = "2";
-	}else{
-		cuatrimestre = "3";
-	}
-
 	var event = schedule.scheduleJob("*/5 * * * *", function() {
-        console.log('This runs every 5 minute');
-        if(cuatrimestre === "1"){
-        	session.endDialog("Recuerde que debe realizar el pago de declaracion cuatrimestral de IVA el "+cuatrimestral)
-        }else if(cuatrimestre === "2"){
-        	session.endDialog("Recuerde que debe realizar el pago de declaracion cuatrimestral de IVA el "+cuatrimestral)
-        }else{
-        	session.endDialog("Recuerde que debe realizar el pago de declaracion cuatrimestral de IVA el "+cuatrimestral)
-        }
+		console.log('This runs every 5 minute');
+		var currentDate = new Date();
+		var nMonth = currentDate.getMonth();
+		var consulta;
+		var idUsuario = session.userData.idUsuario;
+
+		connection.beginTransaction(function(err) {
+			if (err) {
+				console.log("ERROR 1-------->",err); 
+				throw err; 
+			}
+			
+			connection.query('SELECT documento FROM detalle_usuario WHERE id_usuario = ?', idUsuario, function(err, result) {
+				console.log("ERROR: ----------------> "+err+" ||| RESULT ------------>:"+result);
+				if (err) { 
+					console.log("ERROR 2:------------>",err);
+					throw err;
+				}
+				if(result.length > 0){
+		            console.log("RESULT DOCUMENTO----------->", result);
+		            documento = result[0].documento;
+		            var lastChar = documento.substr(documento.length-1);
+		            var cuatrimestre;
+		            if(nMonth >= 0 && nMonth <= 3){
+						cuatrimestre = "1";
+					}else if(nMonth > 3 && nMonth <= 7){
+						cuatrimestre = "2";
+					}else{
+						cuatrimestre = "3";
+					}
+					connection.query("SELECT cuatrimestre"+cuatrimestre+" FROM declaracion WHERE ult_digito = ?",lastChar, function(err, result, fields) {
+		            	console.log("ERROR SEGUNDO QUERY---->",err);
+		        		if (err) throw err;
+		        		 if(cuatrimestre === "1"){
+				        	session.endDialog("Recuerde que debe realizar el pago de declaracion cuatrimestral de IVA el "+result[0].cuatrimestre1)
+				        }else if(cuatrimestre === "2"){
+				        	session.endDialog("Recuerde que debe realizar el pago de declaracion cuatrimestral de IVA el "+result[0].cuatrimestre2)
+				        }else{
+				        	session.endDialog("Recuerde que debe realizar el pago de declaracion cuatrimestral de IVA el "+result[0].cuatrimestre3)
+				        }
+		
+		            });
+				}
+				connection.end();
+			});
+		});
     });
 }
