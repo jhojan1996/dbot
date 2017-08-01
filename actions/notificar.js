1eb200319b0f7b669e0bd6ab369b7ac4807e4d18
Innovati1\Jhojan Garcia <jhojan@consultoriagp.co>
2017-08-01 08:44:34 -0500

    pool connections

--- actions/notificar.js
+++ actions/notificar.js
@@ -8,7 +8,8 @@ var connector = new builder.ChatConnector({
 });
 var bot = new builder.UniversalBot(connector);
 
-var connection = mysql.createConnection({
+var pool = mysql.createPool({
+	connectionLimit : 10,
     host     : 'us-cdbr-azure-southcentral-f.cloudapp.net',
     user     : 'bdfb18a7b2c383',
     password : '669f8c04',
@@ -71,63 +72,48 @@ function programarNoti(session){
 		var consulta;
 		var idUsuario = session.userData.idUsuario;
 		var address = session.message.address;
+		var documento;
 
-		connection.connect(function(err) {
-		    if (err) {
-		        console.error('error connecting: ' + err.stack);
-		        return;
-		    }
-		    console.log('connected as id ' + connection.threadId);
-		});
-
-		connection.beginTransaction(function(err) {
-			if (err) {
-				console.log("ERROR 1-------->",err); 
-				throw err; 
-			}
-			
-			connection.query('SELECT documento FROM detalle_usuario WHERE id_usuario = ?', idUsuario, function(err, result) {
-				console.log("ERROR: ----------------> "+err+" ||| RESULT ------------>:"+result);
-				if (err) { 
-					console.log("ERROR 2:------------>",err);
-					throw err;
-				}
-				if(result.length > 0){
-		            console.log("RESULT DOCUMENTO----------->", result);
-		            documento = result[0].documento;
-		            var lastChar = documento.substr(documento.length-1);
-		            var cuatrimestre;
-		            if(nMonth >= 0 && nMonth <= 3){
+		pool.getConnection(function(err, connection){
+			connection.query('SELECT documento FROM detalle_usuario WHERE id_usuario = ?',idUsuario, function(err, rows){
+				console.log("ERROR 1--------->", err);
+				if(err) throw err;
+				console.log("RESULT ROWS----->", rows);
+				if (rows.length > 0){
+					documento = rows[0].documento;
+					var lastChar = documento.substr(documento.length-1);
+	            	var cuatrimestre;
+	            	if(nMonth >= 0 && nMonth <= 3){
 						cuatrimestre = "1";
 					}else if(nMonth > 3 && nMonth <= 7){
 						cuatrimestre = "2";
 					}else{
 						cuatrimestre = "3";
 					}
-					connection.query("SELECT cuatrimestre"+cuatrimestre+" FROM declaracion WHERE ult_digito = ?",lastChar, function(err, result, fields) {
-		            	console.log("ERROR SEGUNDO QUERY---->",err);
+
+					connection.query("SELECT cuatrimestre"+cuatrimestre+" FROM declaracion WHERE ult_digito = ?", lastChar, function(err,rows){
+						console.log("ERROR 2---->",err);
 		        		if (err) throw err;
-		        		console.log("ERROR TERCER QUERY---->",err);
+		        		console.log("ERROR 3---->",err);
 		        		 if(cuatrimestre === "1"){
 				        	var msg = new builder.Message().address(address);
-						    msg.text("Recuerde que debe realizar el pago de declaracion cuatrimestral de IVA el "+result[0].cuatrimestre1);
+						    msg.text("Recuerde que debe realizar el pago de declaracion cuatrimestral de IVA en la fecha "+result[0].cuatrimestre1);
 						    msg.textLocale('es-ES');
 						    bot.send(msg);
 				        }else if(cuatrimestre === "2"){
 				        	var msg = new builder.Message().address(address);
-						    msg.text("Recuerde que debe realizar el pago de declaracion cuatrimestral de IVA el "+result[0].cuatrimestre2);
+						    msg.text("Recuerde que debe realizar el pago de declaracion cuatrimestral de IVA en la fecha "+result[0].cuatrimestre2);
 						    msg.textLocale('es-ES');
 						    bot.send(msg);
 				        }else{
 				        	var msg = new builder.Message().address(address);
-						    msg.text("Recuerde que debe realizar el pago de declaracion cuatrimestral de IVA el "+result[0].cuatrimestre3);
+						    msg.text("Recuerde que debe realizar el pago de declaracion cuatrimestral de IVA en la fecha "+result[0].cuatrimestre3);
 						    msg.textLocale('es-ES');
 						    bot.send(msg);
 				        }
-		
-		            });
+				        connection.release();
+					});
 				}
-				connection.end();
 			});
 		});
     });
